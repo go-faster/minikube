5821cd077fe9b1f097a5556dce31c8c6c4a4730c
Aleksandr Razumov <ernado@go-faster.org>
2024-01-12 23:55:50 +0300

    feat(iso): add portoshim

---
+++ deploy/iso/minikube-iso/arch/x86_64/package/portoshim-bin/portoshim-bin.mk
@@ -0,0 +1,29 @@
+################################################################################
+#
+# portoshim-bin
+#
+################################################################################
+
+PORTOSHIM_BIN_VERSION = v1.0.9-alpha.2
+PORTOSHIM_BIN_SITE = https://github.com/go-faster/portoshim/releases/download/$(PORTOSHIM_BIN_VERSION)
+PORTOSHIM_BIN_SOURCE = portoshim_focal_$(PORTOSHIM_BIN_VERSION)_amd64.tgz
+
+define PORTOSHIM_BIN_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 \
+		$(@D)/portoshim \
+		$(TARGET_DIR)/sbin/portoshim
+	$(INSTALL) -D -m 0755 \
+		$(@D)/logshim \
+		$(TARGET_DIR)/sbin/logshim
+	$(INSTALL) -D -m 644 \
+		$(PORTOSHIM_BIN_PKGDIR)/crictl.yaml \
+		$(TARGET_DIR)/etc/crictl.yaml
+endef
+
+define PORTOSHIM_BIN_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 644 \
+		$(PORTOSHIM_BIN_PKGDIR)/portoshim.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/portoshim.service
+endef
+
+$(eval $(generic-package))
